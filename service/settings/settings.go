d09e181256b43a0b6ed9df2bd7e8a768e02acb8d
Duy <duynguyenngoc@hotmail.com>
2021-02-27 11:46:49 +0700

    add config

--- service/settings/settings.go
+++ service/settings/settings.go
@@ -8,57 +8,40 @@ package settings
 import (
 	"log"
 	"github.com/spf13/viper"
-	"fmt"
 )
 
+// Global variable
+var Env Config
+
+
+// define config
 type Config struct {
 	Project struct {
-		Name        string `json:"name"`
-		Environment string `json:"environment"`
+		Name        string `yaml:"name"`
+		Environment string `yaml:"environment"`
+		SecretKey string `yaml:"secret_key"`
 	} `yaml:"project"`
 
 	Server struct {
-		Host 	 string `json: "host"`
-		Port 	 int `json:"port"`
+		Host 	 string `yaml: "host"`
+		Port 	 int `yaml:"port"`
 	} `yaml:"server"`
 
 	Database struct {
-		Host     string `json:"host"`
-		Port     int `json:"port"`
-		User 	 string `json:"user"`
-		Pass string `json:"pass"`
-		Database string `json:"database"`
-		Type     string `json:"type"`
+		Host     string `yaml:"host"`
+		Port     int `yaml:"port"`
+		User 	 string `yaml:"user"`
+		Pass 	 string `yaml:"pass"`
+		Name 	 string `yaml:"name"`
+		Type     string `yaml:"type"`
 	} `yaml:"database"`
 }
-
-
-func GetEnv() (config Config) {
-	/* 
-		function load final_env inside variable Env
-	*/
-	viper.AddConfigPath(".")
-	viper.SetConfigName("final_env")
-	viper.SetConfigType("json")
-	viper.AutomaticEnv()
-	err := viper.ReadInConfig()
-    if err != nil {
-		log.Println("Error when read config File!")
-
-    }
-	viper.Unmarshal(&config)
-    return
-}
-
 
 func InitialViperWriteCofig(path string, fileName string, fileType string) {
 	/*
         It is a basic viber to make application can make final_file environment for .yaml, .toml ...
         Just only run one time when starting server.
     */
-
-	//  Config structure
-	var conf Config
 
 	// Set the file name of the configurations file
 	viper.SetConfigName(fileName)
@@ -74,13 +57,12 @@ func InitialViperWriteCofig(path string, fileName string, fileType string) {
 
 	// Handle read config failed
 	if err := viper.ReadInConfig(); err != nil {
-		fmt.Printf("Error reading config file, %s", err)
+		log.Panicf("Unable read Flie Config, %v", err)
 	}
 
 	// handle write file to structure config
-	err := viper.Unmarshal(&conf)
-	if err != nil {
-		fmt.Printf("Unable to decode into struct, %v", err)
+	if err := viper.Unmarshal(&Env); err != nil {
+		log.Panicf("Unable to decode into struct, %v", err)
 	}
-	viper.WriteConfigAs("./final_env.json")
+	return
 }
